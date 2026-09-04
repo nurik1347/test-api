3cff96afc6973216340def48d6da3dfc6e74c84b
nurik1347 <nurikmirzaev1747@gmail.com>
2026-09-04 11:09:19 +0500

    done

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-
 const users = []
+
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get('/users', (req, res) => {
+    res.json({ users });
 });
 
-app.get("/users", (req, res) => {
-    res.json({ users })
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
 
+    const user = {
+        id: users.length + 1,
+        name: name,
+        email: email,
+        password: password
+    };
+
+    users.push(user);
+
+    res.json({
+        message: 'User registered successfully',
+        user: user
+    })
+})
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    const user = users.find()
 })
 
 
 app.listen(3000, () => {
-    console.log('Server ishlavotti');
-});
-
-
-app.post("/users", (req, res) => {
-    console.log(req.body);
-
-    users.push(req.body);
-    res.json({
-        message: "User created",
-        user: req.body
-    });
+    console.log('Server is running on port 3000');
 });
